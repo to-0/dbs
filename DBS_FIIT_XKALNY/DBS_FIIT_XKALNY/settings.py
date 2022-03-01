1818f3bd166f262b3a8b9461120c0fe0ee35c747
Tomáš Kalný <xkalny@stuba.sk>
2022-03-01 14:37:59 +0100

    Allowed host

--- DBS_FIIT_XKALNY/DBS_FIIT_XKALNY/settings.py
+++ DBS_FIIT_XKALNY/DBS_FIIT_XKALNY/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fiit-dbs-xkalny-app.azurewebsites.net']
 
 
 # Application definition
